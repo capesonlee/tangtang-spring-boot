fe8c42b29b82a823302c82f9bcd5af2266893bcc
lijuyong <lijuyong@youbangsoft.com>
2017-02-20 22:07:16 +0800

    调整模板的路径和名字 1. 测试用例更合理（hello，不需要登录） 2. 检验template下面的路径/user/test.html 是否合理。

--- src/main/java/com/lijuyong/Infra/MvcConfig.java
+++ src/main/java/com/lijuyong/Infra/MvcConfig.java
@@ -13,7 +13,7 @@ public class MvcConfig extends WebMvcConfigurerAdapter {
     public void addViewControllers(ViewControllerRegistry registry) {
         //registry.addViewController("/home").setViewName("home");
         //registry.addViewController("/").setViewName("home");
-        registry.addViewController("/hello").setViewName("hello");
+        registry.addViewController("/user/test").setViewName("user/test");
         registry.addViewController("/login").setViewName("login");
     }
 }
