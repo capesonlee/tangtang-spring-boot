8c8a0266f26035e0b316ed79c18911cb7d690202
lijuyong <lijuyong@youbangsoft.com>
2017-02-20 22:00:08 +0800

    自定义登录退action 自定义登录退出成功返回值 user/hello
    注意：必须使用post去退出才能成功。

--- src/main/java/com/lijuyong/Controller/SecurityController.java
+++ src/main/java/com/lijuyong/Controller/SecurityController.java
@@ -2,6 +2,7 @@ package com.lijuyong.Controller;
 
 import com.lijuyong.Domain.Model.JsonResult;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -16,7 +17,7 @@ public class SecurityController {
         return new JsonResult(123,"好啦");
     }
 
-    @RequestMapping("/logout")
+    @RequestMapping(value = "/logout",method= RequestMethod.POST)
     public JsonResult logout(){
         return  new JsonResult(312,"退出成功");
     }
